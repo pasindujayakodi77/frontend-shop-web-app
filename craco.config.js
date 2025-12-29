2cbced52693b9e58d1b0c11867b714a414c9bdf1
pasindujayakoi77 <pasindujaya687@gmail.com>
2025-12-29 19:25:17 +0530

    Add ESLint plugin to webpack configuration and update dependencies✏️

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,23 @@
+const tryRequire = (name) => {
+  try {
+    return require(name);
+  } catch (e) {
+    return null;
+  }
+};
+
+const ESLintPlugin = tryRequire('eslint-webpack-plugin');
+
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
+      if (ESLintPlugin) {
+        const hasPlugin = webpackConfig.plugins && webpackConfig.plugins.some((p) => p && p.constructor && p.constructor.name === 'ESLintWebpackPlugin');
+        if (!hasPlugin) {
+          webpackConfig.plugins = webpackConfig.plugins || [];
+          webpackConfig.plugins.push(new ESLintPlugin({ extensions: ['js', 'jsx', 'ts', 'tsx'] }));
+        }
+      }
       return webpackConfig;
     },
   },
